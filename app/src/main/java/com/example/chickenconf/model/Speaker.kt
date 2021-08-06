ea726e3fe5fd90b5c816e5d906119770fe3529dd
Gino Ascencio <gino.francisco@hotmail.com>
2021-08-05 19:26:37 -0500

    App Android

---
+++ app/src/main/java/com/example/chickenconf/model/Speaker.kt
@@ -0,0 +1,12 @@
+package com.example.chichenconf.model
+
+import java.io.Serializable
+
+class Speaker: Serializable {
+    var name = ""
+    var jobtitle = ""
+    var workplace = ""
+    var biography = ""
+    var twitter = ""
+    var image = ""
+}
